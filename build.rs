3024a591dff64dfc2f00d8987685ebe153066c6a
Tomas Olvecky <tomas.olvecky@gmail.com>
2021-08-19 22:07:07 +0200

    Remove hello and routeguide

--- build.rs
+++ build.rs
@@ -1,17 +1,4 @@
-use std::{env, path::PathBuf};
-
 fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
-        .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
-        .unwrap();
-
-    tonic_build::configure()
-        .type_attribute("routeguide.Point", "#[derive(Hash)]")
-        .compile(&["proto/routeguide/route_guide.proto"], &["proto"])
-        .unwrap();
-
     tonic_build::configure()
         .compile(&["proto/jobexecutor/job_executor.proto"], &["proto"])
         .unwrap();
