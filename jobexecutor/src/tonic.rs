486a5dd8887adbcead665fff31ddefe810e4d648
Tomas Olvecky <tomas.olvecky@gmail.com>
2021-09-16 23:11:06 +0200

    Splilt ChildInfoError into several errors

--- jobexecutor/src/tonic.rs
+++ jobexecutor/src/tonic.rs
@@ -1,7 +1,28 @@
 use super::childinfo::*;
 
-impl From<ChildInfoError> for tonic::Status {
-    fn from(err: ChildInfoError) -> Self {
+// TODO use proc macro
+impl From<StopError> for tonic::Status {
+    fn from(err: StopError) -> Self {
+        tonic::Status::internal(err.to_string())
+    }
+}
+impl From<AddClientError> for tonic::Status {
+    fn from(err: AddClientError) -> Self {
+        tonic::Status::internal(err.to_string())
+    }
+}
+impl From<StatusError> for tonic::Status {
+    fn from(err: StatusError) -> Self {
+        tonic::Status::internal(err.to_string())
+    }
+}
+impl From<ChildInfoCreationError> for tonic::Status {
+    fn from(err: ChildInfoCreationError) -> Self {
+        tonic::Status::internal(err.to_string())
+    }
+}
+impl From<ChildInfoCreationWithCGroupError> for tonic::Status {
+    fn from(err: ChildInfoCreationWithCGroupError) -> Self {
         tonic::Status::internal(err.to_string())
     }
 }
